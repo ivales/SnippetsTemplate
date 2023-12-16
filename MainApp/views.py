7bc087db5e823c39cdc22b41d756435306adc1ba
ivales <i.leskin@mail.ru>
2023-12-16 16:41:15 +0300

    Add SnippetForm for Snippet

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,7 +1,7 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
 from MainApp.models import Snippet
-
+from MainApp.forms import SnippetForm
 
 def index_page(request):
     context = {'pagename': 'PythonBin'}
@@ -9,8 +9,15 @@ def index_page(request):
 
 
 def add_snippet_page(request):
-    context = {'pagename': 'Добавление нового сниппета'}
-    return render(request, 'pages/add_snippet.html', context)
+    if request.method == "GET":
+        form = SnippetForm()
+        return render(request, 'pages/add_snippet.html', {'form': form})
+    if request.method == "POST":
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("snippets_list")
+        return render(request, 'add_snippet.html', {'form': form})
 
 
 def snippets_page(request):
@@ -23,3 +30,11 @@ def snippet(request, id):
             lonely_snippet = snippet
     context = {"snippet": lonely_snippet}
     return render(request, 'pages/snippet.html', context)
+
+# def create_snippet(request):
+#    if request.method == "POST":
+#        form = SnippetForm(request.POST)
+#        if form.is_valid():
+#            form.save()
+#            return redirect("snippets_list")
+#        return render(request,'add_snippet.html',{'form': form})
--- Snippets/urls.py
+++ Snippets/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('snippets/add', views.add_snippet_page, name='snippet_add'),
     path('snippets/list', views.snippets_page, name='snippets_list'),
     path('snippet/<int:id>', views.snippet, name='snippet'),
+#    path('snippet/create', views.create_snippet, name='create_snippet')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
